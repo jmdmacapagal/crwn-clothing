a7804a86bb61379eba6a409b10214461a8dc92cf
JM Macapagal <jmdmacapagal@gmail.com>
2020-05-06 03:05:45 +0800

    created temporary homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+
+import HomePage from "./homepage.component";
+
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
