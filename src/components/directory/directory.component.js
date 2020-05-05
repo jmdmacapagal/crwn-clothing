3e6d2c121747107e0f9aca477f5580047e205077
JM Macapagal <jmdmacapagal@gmail.com>
2020-05-06 03:40:12 +0800

    refactor, created component

---
+++ src/components/directory/directory.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./directory.styles.scss";
+
+const Directory = ({ children }) => {
+  return <div className="directory">{children}</div>;
+};
+
+export default Directory;
--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,47 +1,31 @@
 import React from "react";
 import "./homepage.styles.scss";
 
+const items = [
+  {
+    title: "Hats",
+    subtitle: "Shop Now",
+  },
+  {
+    title: "Jackets",
+    subtitle: "Shop Now",
+  },
+  {
+    title: "Sneakers",
+    subtitle: "Shop Now",
+  },
+  {
+    title: "Womens",
+    subtitle: "Shop Now",
+  },
+  {
+    title: "Mens",
+    subtitle: "Shop Now",
+  },
+];
+
 const HomePage = () => {
-  return (
-    <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="homepage"></div>;
 };
 
 export default HomePage;
