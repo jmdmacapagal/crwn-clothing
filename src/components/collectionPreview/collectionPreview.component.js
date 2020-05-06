bc547a376d128e631ccf1a6b13a0eac4dde320aa
JM Macapagal <jmdmacapagal@gmail.com>
2020-05-06 16:02:30 +0800

    styled collectionItem and collectionPreview

--- src/components/collectionPreview/collectionPreview.component.js
+++ src/components/collectionPreview/collectionPreview.component.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./collectionPreview.styles.scss";
+import CollectionItem from "../collectionItem/collectionItem.component";
 
 const CollectionPreview = ({ title, items }) => {
   return (
-    <div className="preview-collection">
+    <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
           .map((item) => {
-            return <div key={item.id}>{item.name}</div>;
+            return <CollectionItem key={item.id} {...item} />;
           })}
       </div>
     </div>
