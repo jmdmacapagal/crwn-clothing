b39820ca907ad02bb43a4cba2cb541bd27e8b5a0
JM Macapagal <jmdmacapagal@gmail.com>
2020-05-06 12:02:28 +0800

    implemented react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Switch, Route } from "react-router-dom";
 import HomePage from "../src/pages/homepage/homepage.component";
 
 import "./App.css";
@@ -7,7 +7,9 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
