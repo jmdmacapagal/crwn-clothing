d52c554d2582dd8a6f97bbef35c3ed68efb2beb7
JM Macapagal <jmdmacapagal@gmail.com>
2020-05-09 16:39:02 +0800

    sign up settings

--- src/pages/signInAndSignUp/signInAndSignUp.component.js
+++ src/pages/signInAndSignUp/signInAndSignUp.component.js
@@ -1,10 +1,13 @@
 import React from "react";
 import SignIn from "../../components/signIn/signIn.component";
+import SignUp from "../../components/signUp/signUp.component";
+import "./signInAndSignUp.styles.scss";
 
 const SignInAndSignUp = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
