70a3ef506e34d0dd149cd84a722bf07f05401e48
JM Macapagal <jmdmacapagal@gmail.com>
2020-05-10 02:16:22 +0800

    addItem on state

--- src/components/collectionItem/collectionItem.component.js
+++ src/components/collectionItem/collectionItem.component.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./collectionItem.styles.scss";
 import CustomButton from "../cusomButton/customButton.component";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -10,9 +13,11 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton inverted>Add to cart</CustomButton>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+export default connect(null, { addItem })(CollectionItem);
