bb02da7396997b26461c89facb8a5613dfb9c4fa
JM Macapagal <jmdmacapagal@gmail.com>
2020-05-11 23:35:54 +0800

    increase and decrease functionality in checkout

--- src/components/checkoutItem/checkoutItem.component.js
+++ src/components/checkoutItem/checkoutItem.component.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./checkoutItem.styles.scss";
 import { connect } from "react-redux";
-import { deleteItem } from "../../redux/cart/cart.actions";
+import { deleteItem, addItem, removeItem } from "../../redux/cart/cart.actions";
 
-const CheckoutItem = ({
-  cartItem: { id, name, imageUrl, price, quantity },
-  deleteItem,
-}) => {
+const CheckoutItem = ({ cartItem, deleteItem, addItem, removeItem }) => {
+  const { id, name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#60;
+        </div>
+        <span>{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#62;
+        </div>
+      </span>
 
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => deleteItem(id)}>
@@ -23,4 +29,4 @@ const CheckoutItem = ({
   );
 };
 
-export default connect(null, { deleteItem })(CheckoutItem);
+export default connect(null, { deleteItem, addItem, removeItem })(CheckoutItem);
